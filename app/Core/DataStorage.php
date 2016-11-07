100d09ffce29543e03456000030b399d8e83c765
Ihor Sobchenko <sobchenko.ihor@gmail.com>
2016-11-07 11:09:08 +0200

    Added 'yml' for config file

--- app/Core/DataStorage.php
+++ app/Core/DataStorage.php
@@ -6,10 +6,14 @@ class DataStorage
 {
     private $handler;
 
-    public function __construct($db_host, $db_name, $db_user, $db_pass)
+    public function __construct($db_host, $db_port, $db_name, $db_user, $db_pass)
     {
         try {
-            $this->handler = new \PDO("mysql:host={$db_host};dbname={$db_name};charset=UTF8", $db_user, $db_pass);
+            $this->handler = new \PDO(
+                "mysql:host={$db_host};port={$db_port};dbname={$db_name};charset=UTF8",
+                $db_user,
+                $db_pass
+            );
             $this->handler->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         } catch (\PDOException $e) {
             print "Error!: " . $e->getMessage() . "<br/>";
--- app/init.php
+++ app/init.php
@@ -2,5 +2,21 @@
 
 require_once  __DIR__ . '/../vendor/autoload.php';
 
-//$db = new Core\DataStorage('localhost', 'root', 'test', 'test');
-//var_dump($db);
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Yaml\Exception\ParseException;
+
+try {
+    $params = Yaml::parse(file_get_contents(__DIR__ . '/config/parameters.yml'));
+} catch (ParseException $e) {
+    printf("Unable to parse the YAML string: %s", $e->getMessage());
+}
+
+
+$db = new Core\DataStorage(
+    $params['parameters']['db_host'],
+    $params['parameters']['db_port'],
+    $params['parameters']['db_name'],
+    $params['parameters']['db_user'],
+    $params['parameters']['db_pass']
+);
+
